195dcd45cf353a18b60aefcd8326da48df1e5eff
Erol Mehmed <erol.mehmed07@gmail.com>
2026-03-27 20:15:24 +0200

    initial project setup with basic structure and layout files

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace task_flow.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+  public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+    : base(options)
+  {
+  }
+}
